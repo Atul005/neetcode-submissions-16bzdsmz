14890b790a8c4ee5c7e2c2041d2970d98b7a507c
Atul Yadav <yadavatul005@gmail.com>
2026-05-07 12:36:57 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.java
@@ -0,0 +1,96 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] adj = new ArrayList[numCourses];
+        
+        for(int i=0;i<numCourses;i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        int[] inDegree = new int[numCourses];
+        for(int[] pre : prerequisites){
+            adj[pre[1]].add(pre[0]); 
+            inDegree[pre[0]]++;   
+        }
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        int[] res = new int[numCourses];
+        int idx=0;
+        while(!queue.isEmpty()){
+            int polled = queue.poll();
+            res[idx] = polled;
+            idx++;
+            for(int neighbour : adj[polled]){
+                inDegree[neighbour]--;
+                if(inDegree[neighbour] == 0){
+                    queue.add(neighbour);
+                }
+            }
+        }
+
+        if(idx != numCourses){
+            return new int[0];
+        }
+        
+        return res;
+    }
+
+
+
+
+    // public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // ArrayList<Integer>[] adj = new ArrayList[numCourses];
+        
+        // for(int i=0;i<numCourses;i++){
+        //     adj[i] = new ArrayList<>();
+        // }
+
+        // for(int[] pre : prerequisites){
+        //     adj[pre[1]].add(pre[0]);    
+        // }
+
+    //     ArrayList<Integer> res = new ArrayList<>();
+
+    //     boolean[] visited = new boolean[numCourses];
+    //     boolean[] recStack = new boolean[numCourses];
+
+    //     for(int i=0; i<numCourses; i++){
+    //         if(!visited[i]){
+    //             if(checkCycle(i, visited, recStack, adj, res)){
+    //                 return new int[0];
+    //             }
+    //         }
+    //     }
+        
+    //     Collections.reverse(res);
+
+    //     int[] arr = new int[res.size()];
+    //     for (int i = 0; i < res.size(); i++) {
+    //         arr[i] = res.get(i);
+    //     }
+    //     return arr;
+    // }
+
+    // public boolean checkCycle(int source, boolean[] visited, boolean[] recStack, ArrayList<Integer>[] adj, ArrayList<Integer> res){
+    //     visited[source] = true;
+    //     recStack[source] = true;
+    //     for(int neighbour : adj[source]){
+    //         if(!visited[neighbour]){
+    //             if(checkCycle(neighbour, visited, recStack, adj, res)){
+    //                 return true;
+    //             }
+    //         }else if(recStack[neighbour]){
+    //             return true;
+    //         }
+    //     }
+    //     recStack[source] = false;
+    //     res.add(source);
+    //     return false;
+    // }
+    
+}
