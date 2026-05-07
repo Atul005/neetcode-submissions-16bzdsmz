29b9d65d7dd9100cd6485d73cf01ad8628347d78
Atul Yadav <yadavatul005@gmail.com>
2026-05-07 10:34:50 +0530

    Add: subsets - submission-9

---
+++ Data Structures & Algorithms/subsets/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        sets(0, nums, res, new ArrayList<>());
+        // System.out.println(res);
+        return res;
+    }
+
+    public void sets(int idx, int[] nums, List<List<Integer>> res, List<Integer> l){
+        res.add(new ArrayList<>(l));
+
+        for(int i=idx;i<nums.length;i++){
+            l.add(nums[i]);
+            sets(i+1,nums, res, l);
+            l.remove(l.size() - 1);
+        }
+
+        // if(idx == nums.length){
+        //     res.add(new ArrayList<>(l));
+        //     return;
+        // }
+
+        // l.add(nums[idx]);
+        // sets(idx+1, nums, res,l);
+        // l.remove(l.size()-1);
+        // sets(idx+1, nums, res, l);
+    }
+}
