6cf4f678661b26f352637cd720030bae5134c641
Atul Yadav <yadavatul005@gmail.com>
2026-06-05 21:35:25 +0530

    Add: find-median-in-a-data-stream - submission-6

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-6.java
@@ -0,0 +1,31 @@
+class MedianFinder {
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }        
+    }
+    
+    public double findMedian() {
+        int size1 = maxHeap.size();
+        int size2 = minHeap.size();
+        
+        if(size1 == 0){
+            return 0.0;
+        }else if(size2 == 0 || (size1 + size2) % 2 == 1){
+            return maxHeap.peek(); 
+        } else{
+            return (double)(maxHeap.peek() + minHeap.peek()) / 2;
+        }
+    }
+
+}
