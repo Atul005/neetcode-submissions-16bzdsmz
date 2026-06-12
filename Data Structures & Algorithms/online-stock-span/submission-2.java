688243431bd6ace62ba57de471a966c1fc0f9855
Atul Yadav <yadavatul005@gmail.com>
2026-06-12 13:01:19 +0530

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.java
@@ -0,0 +1,55 @@
+class StockSpanner {
+
+    List<Integer> prices;
+    Deque<Pair> stack;
+
+    public StockSpanner() {
+        prices = new ArrayList<>();
+        stack = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+        int span=1;
+        boolean isDone = false;
+        if(stack.isEmpty()){
+            stack.push(new Pair(price, 1));
+            return span;
+        }
+
+        while(!stack.isEmpty() && !isDone){
+            if(price >= stack.peek().a){
+                span += stack.pop().b;
+            }else {
+                stack.push(new Pair(price, span));
+                isDone = true;
+            }
+        }
+
+        if(stack.isEmpty() && !isDone){
+            stack.push(new Pair(price, span));
+        }
+
+        
+        // if(isDone){
+        //     stack.push(new Pair(price, span));
+        // }
+
+        return span;
+    }
+}
+
+
+class Pair{
+    int a;
+    int b;
+
+    Pair(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
