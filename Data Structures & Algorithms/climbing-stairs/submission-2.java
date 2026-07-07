a09c6e47c750b37211e7f2451d6a3229f062a36e
Atul Yadav <yadavatul005@gmail.com>
2026-07-07 11:56:55 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 1){
+            return 1;
+        }
+
+        int[] tab = new int[n+1];
+        tab[0] = 1;
+        tab[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            tab[i] = tab[i-1] + tab[i-2];
+        }
+
+        return tab[n];
+    }
+}
